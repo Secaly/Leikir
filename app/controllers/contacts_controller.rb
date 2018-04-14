752a05ee6f793bbdc742a3004bca81321a3e0965
Secaly <kevin.ledieu.fr@gmail.com>
2018-04-14 19:45:32 +0200

    add views to create and see contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,3 +1,26 @@
 class ContactsController < ApplicationController
-  
+
+  def index
+    @contacts = Contact.all.order("created_at DESC")
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(item_params)
+    if @contact.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def item_params
+      params.require(:contact).permit(:first_name, :last_name)
+    end
+
 end
