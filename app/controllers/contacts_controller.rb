23ee14fff4c68fc698d87e51f248c99198952848
Secaly <kevin.ledieu.fr@gmail.com>
2018-04-15 20:51:20 +0200

    add show action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,11 @@
 class ContactsController < ApplicationController
+  before_action :find_contact, only: [:show]
 
   def index
     @contacts = Contact.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
@@ -21,6 +25,10 @@ class ContactsController < ApplicationController
 
     def item_params
       params.require(:contact).permit(:first_name, :last_name)
+    end
+
+    def find_contact
+      @contact = Contact.find(params[:id])
     end
 
 end
